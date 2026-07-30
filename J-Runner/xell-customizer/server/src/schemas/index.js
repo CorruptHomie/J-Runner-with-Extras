3f752cfa39b1fa9f73100e9b1ef5a262da008d5d
Scallywag <76604672+ScallywagDude@users.noreply.github.com>
2026-07-30 14:16:24 -0400

    Delete J-Runner directory

--- J-Runner/xell-customizer/server/src/schemas/index.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// Ported unchanged from xell-customizer-api/src/schemas/index.ts - Zod doesn't care what
-// JS runtime it's under.
-export const generateSchema = z.object({
-  background_color: z.string().optional(),
-  foreground_color: z.string().optional(),
-  ascii_art: z.string().optional(),
-});
